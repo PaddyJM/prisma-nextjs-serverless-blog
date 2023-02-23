51931e321d6a6af20dff5a6d313495ff38fddb9b
Patrick Morton <patrickjamesmorton@gmail.com>
2023-02-23 11:25:26 +0000

    initial commit

---
+++ src/pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth/next";
+import GitHubProvider from "next-auth/providers/github";
+
+const prisma = new PrismaClient();
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET ?? "",
+};
